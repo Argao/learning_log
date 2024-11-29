347766b8bf017d9a2092093f4cd8f411c85e6b2e
Argao <jgabrielaragao@gmail.com>
2024-11-28 22:45:00 -0300

    fim do dia

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Topic
+
 
 # Create your views here.
+
+def index(request):
+    return render(request, 'learning_logs/index.html')
+
+
+def topics(request):
+    topic = Topic.objects.order_by('date_added')
+    context = {'topics': topic}
+
+    return render(request, 'learning_logs/topics.html', context)
